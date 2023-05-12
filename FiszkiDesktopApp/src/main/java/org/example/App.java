de15a4ed283d3815ab0970e7a0557b814951fd08
DwiN3 <dwinicht333@gmail.com>
2023-05-12 13:17:01 +0200

    Zmiana rozmiaru

--- FiszkiDesktopApp/src/main/java/org/example/App.java
+++ FiszkiDesktopApp/src/main/java/org/example/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("activity_first_screen"), 640, 480);
+        scene = new Scene(loadFXML("activity_first_screen"), 850, 600);
         stage.setScene(scene);
         stage.show();
     }
