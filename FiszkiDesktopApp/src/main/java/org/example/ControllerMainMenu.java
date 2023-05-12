0adb5a0e6176d3ad6b18e1d692c8c142ed90c376
DwiN3 <dwinicht333@gmail.com>
2023-05-12 13:13:29 +0200

    Zmiana nazw i  konfiguracja projektu

--- FiszkiDesktopApp/src/main/java/org/example/ControllerMainMenu.java
+++ FiszkiDesktopApp/src/main/java/org/example/ControllerMainMenu.java
@@ -3,10 +3,10 @@ package org.example;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class SecondaryController {
+public class ControllerMainMenu {
 
     @FXML
     private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+        App.setRoot("activity_first_screen");
     }
 }
