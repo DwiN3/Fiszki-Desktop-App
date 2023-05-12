b002e60dd4df86e5c5d8a7632010e351bf5eb6a0
DwiN3 <dwinicht333@gmail.com>
2023-05-12 13:29:05 +0200

    delikatnie zmiany

--- FiszkiDesktopApp/src/main/java/org/example/ControllerFirstScreen.java
+++ FiszkiDesktopApp/src/main/java/org/example/ControllerFirstScreen.java
@@ -2,9 +2,11 @@ package org.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class ControllerFirstScreen {
-
+    
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("activity_main_menu");
