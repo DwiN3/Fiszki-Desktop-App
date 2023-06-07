4141958251a7fa2999c0e75c6aac9fab51406829
DwiN3 <dwinicht333@gmail.com>
2023-06-07 20:53:43 +0200

    zmiana w app

--- FiszkiDesktopApp/src/main/java/app/App.java
+++ FiszkiDesktopApp/src/main/java/app/App.java
@@ -6,15 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.Objects;
 
 /**
- * JavaFX App
+ *  Fiszki App
  */
 public class App extends Application {
-
     private static Scene scene;
 
     @Override
@@ -38,5 +36,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
